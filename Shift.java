12010319fdcd6d4311f96c2e669cd94ffd32000d
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 22:42:38 +0530

    Add files via upload
    this program displays the right and left shift.

---
+++ Shift.java
@@ -0,0 +1,9 @@
+public class Shift {  
+    public static void main( String[ ] args ) {  
+        int x = 5 ;    
+        int y = x << 4 ;  
+        int z = x >> 4 ; 
+        System.out.println( " The left shifted value : " +y) ;
+        System.out.print( " The right shifted value is : " +z) ;    
+    }  
+} 
